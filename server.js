c0af53ee22834ea7161f244466c2da5142656076
aliosmansahin <aliosmsahin01@gmail.com>
2026-06-18 01:47:35 +0300

    initial commit
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const myAds = [
+    {
+        ad_id: "adrendev_dark",
+        image_url: "https://adrads.adrendev.com/assets/images/adrendev_ad_dark.webp",
+        target_url: "https://adrendev.com",
+        style: {
+            theme: "dark"
+        }
+    },
+    {
+        ad_id: "adrednev_light",
+        image_url: "https://adrads.adrendev.com/assets/images/adrendev_ad_light.webp",
+        target_url: "https://adrendev.com",
+        style: {
+            theme: "light"
+        }
+    },
+];
+
+app.get('/api/v1/ads/random', (req, res) => {
+
+    const requestedTheme = req.query.theme;
+
+    let filteredAds = myAds;
+    if (requestedTheme) {
+        filteredAds = myAds.filter(ad => ad.style.theme === requestedTheme);
+    }
+
+    if (filteredAds.length === 0) {
+        filteredAds = myAds;
+    }
+
+    if (filteredAds.length === 0) {
+        return res.status(404).json({ message: "No active ad found." });
+    }
+
+    const randomIndex = Math.floor(Math.random() * filteredAds.length);
+    const selectedAd = filteredAds[randomIndex];
+
+    res.json(selectedAd);
+});
+
+app.listen(PORT, () => {
+    console.log(`adrads started on PORT ${PORT}.`);
+});
